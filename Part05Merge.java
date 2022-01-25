500477dee6d14130f8ce237f23fba52676d50468
whereq <whereq@gmail.com>
2022-01-24 23:21:33 -0500

    Create Part05Merge.java

---
+++ Part05Merge.java
@@ -0,0 +1,40 @@
+package io.pivotal.literx;
+
+//generic imports to help with simpler IDEs (ie tech.io)
+import java.util.*;
+import java.util.function.*;
+import java.time.*;
+
+import io.pivotal.literx.domain.User;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Learn how to merge flux.
+ *
+ * @see <a href="https://tech.io/playgrounds/929/reactive-programming-with-reactor-3/Merge">Merge Question Desc</a>"
+ */
+public class Part05Merge {
+
+//========================================================================================
+
+	// TODO Merge flux1 and flux2 values with interleave
+	Flux<User> mergeFluxWithInterleave(Flux<User> flux1, Flux<User> flux2) {
+		return flux1.mergeWith(flux2);
+	}
+
+//========================================================================================
+
+	// TODO Merge flux1 and flux2 values with no interleave (flux1 values and then flux2 values)
+	Flux<User> mergeFluxWithNoInterleave(Flux<User> flux1, Flux<User> flux2) {
+		return flux1.concatWith(flux2);
+	}
+
+//========================================================================================
+
+	// TODO Create a Flux containing the value of mono1 then the value of mono2
+	Flux<User> createFluxFromMultipleMono(Mono<User> mono1, Mono<User> mono2) {
+		return Flux.concat(mono1, mono2);
+	}
+
+}
