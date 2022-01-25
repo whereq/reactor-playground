dae1fe190cac0b3d8dd9827cea53342b709d24e0
whereq <whereq@gmail.com>
2022-01-25 00:25:49 -0500

    Create Part11BlockingToReactive.java

---
+++ Part11BlockingToReactive.java
@@ -0,0 +1,48 @@
+package io.pivotal.literx;
+
+//generic imports to help with simpler IDEs (ie tech.io)
+import java.util.*;
+import java.util.function.*;
+import java.time.*;
+
+import io.pivotal.literx.domain.User;
+import io.pivotal.literx.repository.BlockingRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * Learn how to call blocking code from Reactive one with adapted concurrency strategy for
+ * blocking code that produces or receives data.
+ *
+ * For those who know RxJava:
+ *  - RxJava subscribeOn = Reactor subscribeOn
+ *  - RxJava observeOn = Reactor publishOn
+ *  - RxJava Schedulers.io <==> Reactor Schedulers.elastic
+ *
+ * @see <a href="https://tech.io/playgrounds/929/reactive-programming-with-reactor-3/BlockingToReactive">BlockingToReactive Questions Desc</a>
+ * @see Flux#subscribeOn(Scheduler)
+ * @see Flux#publishOn(Scheduler)
+ * @see Schedulers
+ */
+public class Part11BlockingToReactive {
+
+//========================================================================================
+
+	// TODO Create a Flux for reading all users from the blocking repository deferred until the flux is subscribed, and run it with an elastic scheduler
+	Flux<User> blockingRepositoryToFlux(BlockingRepository<User> repository) {
+		return Flux.defer(() ->
+			Flux.fromIterable(repository.findAll())
+					.subscribeOn(Schedulers.elastic())
+		);
+	}
+
+//========================================================================================
+
+	// TODO Insert users contained in the Flux parameter in the blocking repository using an elastic scheduler and return a Mono<Void> that signal the end of the operation
+	Mono<Void> fluxToBlockingRepository(Flux<User> flux, BlockingRepository<User> repository) {
+		return flux.publishOn(Schedulers.elastic()).doOnNext(repository::save).then();
+	}
+
+}
